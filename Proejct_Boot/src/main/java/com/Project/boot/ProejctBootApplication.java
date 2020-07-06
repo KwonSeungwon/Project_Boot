4f721e647de3a7ce3f84e2ee042872976ed1b1eb
SeungWonKwon <loveyoo41@gmail.com>
2020-07-06 14:04:37 +0900

    스프링 부트 + 그레들 환경잡기 커밋

---
+++ Proejct_Boot/src/main/java/com/Project/boot/ProejctBootApplication.java
@@ -0,0 +1,13 @@
+package com.Project.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProejctBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProejctBootApplication.class, args);
+	}
+
+}
---
+++ Proejct_Boot/src/test/java/com/Project/boot/ProejctBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProejctBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
