23d59bf85b758434f00b8b650359eacaf39b26ed
SeungWonKwon <loveyoo41@gmail.com>
2020-07-07 17:22:41 +0900

    vue.js 프로젝트 환경설정완료

--- Proejct_Boot/src/main/java/com/Project/boot/ProejctBootApplication.java
+++ Proejct_Boot/src/main/java/com/Project/boot/ProejctBootApplication.java
@@ -2,19 +2,10 @@ package com.Project.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class ProejctBootApplication {
 	
-	@GetMapping("/")
-	public String home() {
-		
-		return "Hello World!";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProejctBootApplication.class, args);
 	}
