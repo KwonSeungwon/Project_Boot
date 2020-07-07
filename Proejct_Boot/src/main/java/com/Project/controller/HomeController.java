b951434467b4ff1be1cdc99b0a7d28ef0d17c931
SeungWonKwon <loveyoo41@gmail.com>
2020-07-07 14:53:45 +0900

    Spring boot Hello World 찍어보기 마무리

--- Proejct_Boot/src/main/java/com/Project/controller/HomeController.java
+++ Proejct_Boot/src/main/java/com/Project/controller/HomeController.java
@@ -1,15 +1,15 @@
 package com.Project.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String home() {
 		
-		return "/index.jsp";
+		return "Hello World!";
 	}
 
 }
